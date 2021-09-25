b761c5ee891eb56fcc77dc3b2f045875dbfc7725
Riya Kumari <riyakumari@wireless-10-104-246-108.umd.edu>
2021-09-24 22:50:09 -0400

    Added functionality to read and display mp4

--- ASN2 - Detecting Surviors AKA Barrels/main.py
+++ ASN2 - Detecting Surviors AKA Barrels/main.py
@@ -1,4 +1,34 @@
 import cv2
+# run it by typing 'python3 main.py' in terminal/command-prompt
+# Opens and reads the video, displaying it in another window
+# TODO: Detect the barrels and draw the rectangles around the barrels
 
 
 input_video_path = './BarrelVideo.mp4'
+cap = cv2.VideoCapture(input_video_path)
+
+if (cap.isOpened() == False):
+    print("Error in opening BarrelVideo.mp4")
+
+
+while (cap.isOpened()):
+    # Capture frame-by-frame
+    vid, frame = cap.read()
+
+    if vid == True:
+        # Display the resulting frame
+        cv2.imshow('BarrelVideo', frame)
+
+        # Press Q on keyboard to  exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    # Breaks the loop
+    else:
+        break
+
+# When everything done, release the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
